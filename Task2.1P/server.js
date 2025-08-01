b1e93e7b3c71b983cbd43859b9c67a9812a005f4
The Herald Sun <samuelryanmason@gmail.com>
2025-08-01 22:53:09 +1000

    Task4.1P

--- Task2.1P/server.js
+++ Task2.1P/server.js
@@ -5,10 +5,10 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const https = require("https")
 
-const mailerSendApiKey = process.env.MAILERSEND_API_KEY
-const mailerSendUrl = "https://api.mailersend.com/v1/email"
-const senderEmail = "test@test-2p0347zxqq3lzdrn.mlsender.net"
-const senderName = "What a great name to be sending emails with"
+// const mailerSendApiKey = process.env.MAILERSEND_API_KEY
+// const mailerSendUrl = "https://api.mailersend.com/v1/email"
+// const senderEmail = "test@test-2p0347zxqq3lzdrn.mlsender.net"
+// const senderName = "What a great name to be sending emails with"
 
 const app = express()
 app.use(bodyParser.urlencoded({extended:true}))
@@ -22,37 +22,40 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html")
 })
 
-app.post('/', async (req, res) => {
-    const firstname = req.body.first_name
-    const lastname = req.body.last_name
-    const email = req.body.email
-    
-    console.log(req.body)
-
-    const emailPayload = {
-        from: {
-            email: senderEmail,
-            name: senderName
-        },
-        to: [
-            {
-                email: email, 
-                name: `${firstname} ${lastname}` 
-            }
-        ],
-        subject: "I love Emailing",
-        text: `Hello ${firstname} ${lastname}`
-    }
-
-    const response = await fetch(mailerSendUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json', 
-            'Authorization': `Bearer ${mailerSendApiKey}`, 
-            'X-Requested-With': 'XMLHttpRequest' 
-        },
-        body: JSON.stringify(emailPayload) 
-    });
-
-    res.status(200).send("Email sent successfully!");
-})
+// app.post('/', async (req, res) => {
+//     const firstname = req.body.first_name
+//     const lastname = req.body.last_name
+//     const email = req.body.email
+//
+//     console.log(req.body)
+//
+//     // const emailPayload = {
+//     //     to: [
+//     //         {
+//     //             email: email, 
+//     //             name: `${firstname} ${lastname}` 
+//     //         }
+//     //     ],
+//     //     subject: "I love Emailing",
+//     //     content: [
+//     //         {
+//     //             type: "text/plain",
+//     //             value: "Welcome to dev@deakin"
+//     //         }
+//     //     ]
+//     // }
+//
+//     const response = await fetch(https://api.sendgrid.com/v3/mail/send, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json', 
+//             'Authorization': `Bearer ${SEND_GRID_API_KEY}`, 
+//             'X-Requested-With': 'XMLHttpRequest' 
+//         },
+//         body: JSON.stringify(emailPayload) 
+//     });
+//
+//     console.log(response)
+//
+//     res.status(200).send("Email sent successfully!");
+// })
