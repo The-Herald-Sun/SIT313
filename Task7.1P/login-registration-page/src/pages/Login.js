3c54dd4a7e1081e291d008c9898af0248c1099e7
The Herald Sun <samuelryanmason@gmail.com>
2025-09-03 14:19:18 +1000

    Styling

--- Task7.1P/login-registration-page/src/pages/Login.js
+++ Task7.1P/login-registration-page/src/pages/Login.js
@@ -46,7 +46,7 @@ export default function Login() {
         <br />
         <button type="submit">Log In</button>
         <br />
-        {error}
+        <span style={{ color: "red" }}>{error}</span>
       </form>
     </div>
   )
